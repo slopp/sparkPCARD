1cb22fbb643b444194d22bffc8ddef8c4b466978
Sean Lopp <sean@rstudio.com>
2016-07-22 15:30:55 -0600

    changes spark dependencies

--- R/sparkapi_dependencies.R
+++ R/sparkapi_dependencies.R
@@ -1,4 +1,4 @@
-spark_dependencies <- function(config,...){
+spark_dependencies <- function(spark_version, scala_version,...){
   sparkapi::spark_dependency(
     packages = c("djgg:PCARD:1.1")
   )
